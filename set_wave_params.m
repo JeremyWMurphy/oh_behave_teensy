314d7da83be11e3a6b9bead86a3b608530ab484d
Jeremy Murphy <JeremyWMurphy@gmail.com>
2025-09-25 12:26:45 -0400

    parameters tab

---
+++ set_wave_params.m
@@ -0,0 +1,8 @@
+function [chan,pulse_type,pulse_len,pulse_amp,pulse_intrvl,pulse_base] = set_wave_params(f)
+chan = f.
+pulse_type = '0';
+pulse_len = '20'; % ms
+pulse_amp = '0';
+pulse_intrvl = '0';
+pulse_reps = '3';
+pulse_base = '0';

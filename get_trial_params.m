06f4d6ed72a4af919636d49362c9f8a5999c90ee
Jeremy Murphy <JeremyWMurphy@gmail.com>
2025-09-26 09:53:09 -0400

    Gui clean up

--- get_trial_params.m
+++ get_trial_params.m
@@ -2,10 +2,10 @@ function [n_trials,baseln,itis,lick_pause_time] = get_trial_params(f)
 
 p = f.Children(1).Children(2).Children(1).Children;
 
-n_trials = p(19).Value;
-baseln = p(21).Value./1000;
-itis = [p(23).Value p(25).Value]./1000;
-lick_pause_time = p(27).Value;
+n_trials = p(20).Value;
+baseln = p(22).Value./1000;
+itis = [p(24).Value p(26).Value]./1000;
+lick_pause_time = p(28).Value;
 
 
 
